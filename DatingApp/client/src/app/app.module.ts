7f020e98c4944d5e89fefc9520ec266efe88ad6d
mazal <mazal@LAPTOP-NUB4I166>
2022-01-09 21:36:15 +0200

    Persisting the login

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,12 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+   
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../models/User';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn : boolean =false;
 
-  constructor() { }
+  constructor(private accountService :AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  logout(){
+    this.accountService.logout();
+    this.loggedIn =false;
+  }
+
+  login(){
+    this.accountService.login(this.model)
+   .subscribe(response =>{
+     console.log(response);
+     this.loggedIn = true;
+   }, error =>{
+     console.log('Failed to login',error)
+   });
+  }
+
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe((user: User | null)=> {
+      this.loggedIn = !!user;
+    });
   }
 
 }
