e4321d8053e63a5e0e57a7647aa5914458257f41
mazal <mazal@LAPTOP-NUB4I166>
2022-02-20 20:18:03 +0200

    .

--- DatingApp/API/Extentions/ClaimsPrinciplalExtension.cs
+++ DatingApp/API/Extentions/ClaimsPrinciplalExtension.cs
@@ -6,6 +6,7 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user){
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
         }
     }
 }
